407d36d30bd8477132b8887161c79b4420bac818
STWNT168 <SATWANT168@GMAIL.COM>
2026-08-18 09:18:51 +0530

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbwqvDnfAC6ICego36HMebIJPm-n2NW_zr_UOa9Hxmyi-KvH5uHARSkCAUD9zl9r1qHDvg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyWEfwne6VxY8E39EfiDqjIMiNjnDO_ybdhPLJ-I-1STJ0Up_6wLiJo6zXf6twEgu5C/exec",
   APP_NAME: "PMV Toolkit Management System",
   SHORT_NAME: "PMV Tracker",
   VERSION: "5.0.7",
